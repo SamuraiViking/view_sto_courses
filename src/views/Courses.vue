3b4bd1e41d7c8a8b411f9970fc66fa8b1d5fa685
Kevin <nelson67@stolaf.edu>
2019-08-04 16:01:28 -0500

    adds user can see courses from 2015-2022

--- src/views/Courses.vue
+++ src/views/Courses.vue
@@ -1,20 +1,45 @@
 <template>
   <div class="test">
     <h1>{{ message }}</h1>
+    <p>{{ num_of_courses }} Search Results</p>
+    <button v-on:click="getCourses('anything')">get courses</button>
+    <div>
+      <div v-for="course in courses">
+        <hr>
+        {{ course.name }}
+        {{ course.id }}
+        <button>Add Course</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <style>
 </style>
-
 <script>
+import axios from "axios";
 export default {
   data: function() {
     return {
-      message: "Welcome to the courses page"
+      message: "Welcome to the courses page",
+      courses: '',
+      num_of_courses: '',
+      type: ''
     };
   },
-  created: function() {},
-  methods: {}
+  created: function() {
+    axios.get('api/courses').then(response => {
+      this.courses = response.data.courses;
+      this.num_of_courses = response.data.count;
+    });
+  },
+  methods: {
+    getCourses: function(input) {
+      axios.get(`api/courses?type=${this.type}`).then(response => {
+        this.courses = response.data.courses;
+        this.num_of_courses = response.data.count;
+      });
+    }
+  }
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,31 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
-    {{ term.year }}
-    {{ term.semester }}
-    <div>{{ termCourses }}</div>
     <div>
-      <button v-on:click="changeSemester(-1)">Prev Semester</button>
-      <button v-on:click="changeSemester(1)">Next Semester</button>
+      {{ term.year }}
+      {{ term.semester }}
+      <div>{{ termCourses }}</div>
+      <div>
+        <button v-on:click="changeSemester(-1)">Prev Semester</button>
+        <button v-on:click="changeSemester(1)">Next Semester</button>
+      </div>
+      <div>
+        <button v-if="year > 2015" v-on:click="changeYear(-1)">Prev Year</button>
+        <button v-on:click="changeYear(1)">Next Year</button>
+      </div>
+      <div>
+        <button v-on:click="changeDraft(-1)">Prev Draft</button>
+        <button v-on:click="changeDraft(-1)">Next Draft</button>
+      </div>
     </div>
     <div>
-      <button v-on:click="changeYear(-1)">Prev Year</button>
-      <button v-on:click="changeYear(1)">Next Year</button>
-    </div>
-    <div>
-      <button v-on:click="changeDraft(-1)">Prev Draft</button>
-      <button v-on:click="changeDraft(-1)">Next Draft</button>
+      <div v-if="coursesAvaiable" v-for="course in courses">
+        <hr>
+        <p>{{ course.name }} {{ course.days }} {{ course.times }} <button>Add Course</button></p>
+      </div>
+      <div v-if="!coursesAvaiable">
+        <p>Not yet available</p>
+      </div>
     </div>
   </div>
 </template>
@@ -29,10 +40,12 @@ export default {
   data: function() {
     return {
       message: "Welcome to Vue.js!",
-      year: 2018,
+      year: 2019,
       semester: 1,
+      courses: '',
       termCourses: 'term courses',
       term: '',
+      coursesAvaiable: true
     };
   },
   created: function() {
@@ -40,17 +53,28 @@ export default {
       this.term = response.data;
       this.term = this.term[0];
     });
+    axios.get('api/courses?type=class').then(response => {
+      this.courses = response.data.courses;
+    });
   },
   methods: {
     changeSemester: function(change) {
-      if (this.semester + change <= 5 && this.semester + change >= 1) {
+      if (this.semester + change > 5 && this.year < 2022) {
+        this.year += 1;
+        this.semester = 1;
+      } else if (this.semester + change < 1 && this.year > 2015) {
+        this.year -= 1;
+        this.semester = 5;
+      } else if (this.semester + change <= 5 && this.semester + change >= 1) {
         this.semester += change;
-        this.getTerm();
       }
+      this.getTerm();
     },
     changeYear: function(change) {
-      if (this.year + change <= 2022 && this.year + change >= 2018) {
+      if (this.year + change <= 2022 && this.year + change >= 2015) {
         this.year += change;
+        console.log(this.year);
+        console.log(this.semester);
         this.getTerm();
       }
     },
@@ -59,6 +83,7 @@ export default {
         this.term = response.data;
         this.term = this.term[0];
       });
+      this.getCourses();
       this.getTermCourses(this.term.id);
     },
     getTermCourses: function(termId) {
@@ -66,6 +91,15 @@ export default {
         this.termCourses = response.data; 
       });
     },
+    getCourses: function() {
+      axios.get(`api/courses?term=${this.year}${this.semester}&type=class`).then(response => {
+        this.coursesAvaiable = true;
+        this.courses = response.data.courses;
+        if (this.courses.length === 0) {
+          this.coursesAvaiable = false;
+        }
+      });
+    }
   }
 };
 </script>
